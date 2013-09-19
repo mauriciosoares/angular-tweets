03c8ca5c390b2fc22a67d3bc4888ce218ee85c18
Mauricio Soares <m.soares2603@gmail.com>
2013-09-19 00:30:30 -0300

    Add angularFire, and its working

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -1,12 +1,18 @@
-NGT.controller('homeCtrl', function($scope) {
-	$scope.tweets = [
-		{
-			user: 'mauriciosoares',
-			tweet: 'Meu primeiro tweet away!'
-		},
-		{
-			user: 'mauriciosoares',
-			tweet: 'Meu segundo tweet away!'
+NGT.controller('homeCtrl', function($scope, angularFire) {
+	var ref = new Firebase('https://angular-tweets.firebaseio.com/tweets');
+
+	$scope.tweets = [];
+
+	$scope.saveTweet = function() {
+		if($scope.newTweet !== '' && $scope.newTweet !== undefined) {
+			$scope.tweets.push({
+				user: 'mauriciosoares',
+				tweet: $scope.newTweet
+			});
+
+			$scope.newTweet = '';
 		}
-	];
+	};
+
+	angularFire(ref, $scope, 'tweets');
 });
